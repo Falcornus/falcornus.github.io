848cfec216311afcf7646855ed28d42e487b217b
Falcornus <zvipromus@i.ua>
2016-09-11 20:47:53 +0300

    update

--- origami/js/app.js
+++ origami/js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
-	var menuheight = $(document).height() - 60
-	$(".menu").height(menuheight);
 
 	$(".nav-bar").click(function(){
 		$(".menu").animate({
 			left: 0
 		}, 200, "linear");
 		$(".nav-over").addClass('nav-over-active');
+		$('html,body').css('overflow-y', 'hidden');
 	});
 	$('.nav-over').click(function(){
 		$(".menu").animate({
 			left: "-250px"
 		}, 200, "linear");
 		$(".nav-over").removeClass('nav-over-active');
+		$('html,body').css('overflow-y', 'visible');
 	});
 
 	var projectWidth = $(".project").width() - 80;
@@ -31,4 +31,14 @@ $(document).ready(function(){
             $('#quoteSlider').removeClass('cS-hidden');
         }
 	});
+	$("#phone").mask("+999 (99) 99-99-999");
+
+	$(".to-top").click(function(){
+		$('html,body').animate({scrollTop:0}, 500);
+	});
 });
+
+var app = angular.module('contactForm', ['ngMessages']);
+app.controller('Main', ['$scope', function($scope){
+	
+}]);
