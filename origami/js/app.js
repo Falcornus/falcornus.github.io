e572181fbb62efda31e79fd8939b1f202b57e7f3
Falcornus <zvipromus@i.ua>
2016-10-02 11:49:05 +0200

    update

--- origami/js/app.js
+++ origami/js/app.js
@@ -45,6 +45,45 @@ $(document).ready(function(){
 	});
 
 
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.header').outerHeight();
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('header').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('header').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+
 	// $(".menu .profile ul").each(function(){
 	// 	var width = 0;
 	// 	var currentUl = $(this);
@@ -62,7 +101,7 @@ $(document).ready(function(){
 	// });
 
 
-	$(".submit").click(function(){
+	$(".menu .profile .profile-menu .off a").click(function(){
 		$(".menu .profile .profile-menu .off").css({'display': 'none'});
 		$(".menu .profile .profile-menu .on").css({'display': 'block'});
 	});
